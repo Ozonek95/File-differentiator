85f926ac36948fc52b20df26b3e5a62bb16dd7f4
Kacper Staszek <kacperstaszek1995@gmail.com>
2019-02-09 15:19:10 +0100

    implemented method recognize in classes implements ExtensionTypeRecognizer. Validation based on : "https://en.wikipedia.org/wiki/Magic_number_(programming)#Magic_numbers_in_files"

--- src/com/filedifferentiator/extensions/GifFileRecognize.java
+++ src/com/filedifferentiator/extensions/GifFileRecognize.java
@@ -1,8 +1,13 @@
 package com.filedifferentiator.extensions;
 
 public class GifFileRecognize implements ExtensionTypeRecognizer {
+    private static final String PREFIX_ONE = "474946383961";
+    private static final String PREFIX_TWO = "474946383761";
     @Override
     public boolean recognize(String hexadecimalFile) {
+        if(hexadecimalFile.startsWith(PREFIX_ONE)||hexadecimalFile.startsWith(PREFIX_TWO)){
+            return true;
+        }
         return false;
     }
 }
--- src/com/filedifferentiator/extensions/JpgFilesRecognize.java
+++ src/com/filedifferentiator/extensions/JpgFilesRecognize.java
@@ -1,8 +1,14 @@
 package com.filedifferentiator.extensions;
 
 public class JpgFilesRecognize implements ExtensionTypeRecognizer {
+
+    private static final String PREFIX = "FFD8";
+    private static final String SUFFIX = "FFD9";
     @Override
     public boolean recognize(String hexadecimalFile) {
+        if(hexadecimalFile.startsWith(PREFIX)&&hexadecimalFile.endsWith(SUFFIX)){
+            return true;
+        }
         return false;
     }
 }
