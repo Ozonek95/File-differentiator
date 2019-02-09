9a4e2b5f57a44e6fc39a4562d87ba4066f159c57
Kacper Staszek <kacperstaszek1995@gmail.com>
2019-02-09 15:07:36 +0100

    created class to convert file to hexadecimal string representation

---
+++ src/com/filedifferentiator/to_hexadecimal_converter/FileToHexadecinalConverter.java
@@ -0,0 +1,31 @@
+package com.filedifferentiator.to_hexadecimal_converter;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileToHexadecinalConverter {
+    private StringBuilder builder = new StringBuilder();
+
+    public String fileToHexadecimal(String filePath){
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream(new File(filePath));
+            int fileLength;
+            byte[] data = new byte[16];
+
+            do {
+                fileLength = fileInputStream.read(data);
+                for (int i=0;i<fileLength;i++){
+                    builder.append(String.format("%02X",data[i]));
+                }
+
+            } while (fileLength!=-1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return builder.toString();
+    }
+}
